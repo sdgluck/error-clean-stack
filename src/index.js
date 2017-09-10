d06a650f5c3977378253f68f4f86b495bc16b0c3
Sam Gluck <sdgluck@gmail.com>
2017-09-10 16:08:09 +0100

    pass through args

--- src/index.js
+++ src/index.js
@@ -8,49 +8,49 @@ function cleanStack (err) {
 class CleanError extends Error {
   constructor (...args) {
     super(...args)
-    this.stack  = cleanStack(new Error().stack)
+    this.stack  = cleanStack(new Error(...args).stack)
   }
 }
 
 class CleanReferenceError extends ReferenceError {
   constructor (...args) {
     super(...args)
-    this.stack  = cleanStack(new ReferenceError().stack)
+    this.stack  = cleanStack(new ReferenceError(...args).stack)
   }
 }
 
 class CleanEvalError extends EvalError {
   constructor (...args) {
     super(...args)
-    this.stack  = cleanStack(new EvalError().stack)
+    this.stack  = cleanStack(new EvalError(...args).stack)
   }
 }
 
 class CleanRangeError extends RangeError {
   constructor (...args) {
     super(...args)
-    this.stack  = cleanStack(new RangeError().stack)
+    this.stack  = cleanStack(new RangeError(...args).stack)
   }
 }
 
 class CleanSyntaxError extends SyntaxError {
   constructor (...args) {
     super(...args)
-    this.stack  = cleanStack(new SyntaxError().stack)
+    this.stack  = cleanStack(new SyntaxError(...args).stack)
   }
 }
 
 class CleanTypeError extends TypeError {
   constructor (...args) {
     super(...args)
-    this.stack  = cleanStack(new TypeError().stack)
+    this.stack  = cleanStack(new TypeError(...args).stack)
   }
 }
 
 class CleanURIError extends URIError {
   constructor (...args) {
     super(...args)
-    this.stack  = cleanStack(new URIError().stack)
+    this.stack  = cleanStack(new URIError(...args).stack)
   }
 }
 
